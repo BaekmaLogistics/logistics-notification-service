f28ef4443bb065408d94db81c954c879786b3aac
kth <pni2396@gmail.com>
2026-08-07 01:35:39 +0900

    feat: Slack Bot API (chat.postMessage) 1:1 DM 발송 연동 및 로드밸런서 추가
    - SlackClientImpl Slack Bot API 1:1 DM 발송 어댑터 구현 및 Feign /chat.postMessage 엔드포인트 연동
    - build.gradle OpenFeign spring-cloud-starter-loadbalancer 의존성 추가

--- src/main/java/com/sparta/logistics/notification/infrastructure/external/client/SlackClientImpl.java
+++ src/main/java/com/sparta/logistics/notification/infrastructure/external/client/SlackClientImpl.java
@@ -1,11 +1,14 @@
 package com.sparta.logistics.notification.infrastructure.external.client;
 
 import com.sparta.logistics.notification.application.command.client.SlackClient;
+import com.sparta.logistics.notification.common.code.ErrorResponseCode;
+import com.sparta.logistics.notification.common.exception.ApiException;
 import com.sparta.logistics.notification.domain.entity.SlackMessage;
 import com.sparta.logistics.notification.infrastructure.feign.client.SlackFeignClient;
 import com.sparta.logistics.notification.infrastructure.feign.dto.SlackFeignRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Slf4j
@@ -14,9 +17,24 @@ import org.springframework.stereotype.Component;
 class SlackClientImpl implements SlackClient {
     private final SlackFeignClient slackFeignClient;
 
+    @Value("${slack.bot.token}")
+    private String botToken;
+
     @Override
-    public void sendSlackMessage(SlackMessage slackMessage) {
-        log.info("Sending message to Slack API: id={}", slackMessage.id());
-        slackFeignClient.sendSlackMessage(new SlackFeignRequest(slackMessage.content()));
+    public void transmitSlackMessage(SlackMessage slackMessage) {
+        validate(slackMessage);
+
+        log.info("Sending Slack DM message: id={}, receiverSlackId={}", slackMessage.id(), slackMessage.receiverSlackId());
+
+        SlackFeignRequest request = new SlackFeignRequest(slackMessage.receiverSlackId(), slackMessage.content());
+        slackFeignClient.sendSlackMessage("Bearer " + botToken, request);
+
+        log.info("Successfully sent Slack DM message: id={}", slackMessage.id());
+    }
+
+    private void validate(SlackMessage slackMessage) {
+        if (slackMessage == null || slackMessage.receiverSlackId() == null || slackMessage.receiverSlackId().isBlank()) {
+            throw new ApiException(ErrorResponseCode.INVALID_REQUEST, "수신자의 유효한 Slack ID가 존재하지 않습니다.");
+        }
     }
 }
--- src/main/java/com/sparta/logistics/notification/infrastructure/feign/client/SlackFeignClient.java
+++ src/main/java/com/sparta/logistics/notification/infrastructure/feign/client/SlackFeignClient.java
@@ -4,10 +4,14 @@ import com.sparta.logistics.notification.infrastructure.feign.dto.SlackFeignRequ
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 
 @FeignClient(name = "slackFeignClient", url = "${slack.api.url}")
 public interface SlackFeignClient {
 
-    @PostMapping
-    void sendSlackMessage(@RequestBody SlackFeignRequest request);
+    @PostMapping("/chat.postMessage")
+    void sendSlackMessage(
+            @RequestHeader("Authorization") String token,
+            @RequestBody SlackFeignRequest request
+    );
 }
