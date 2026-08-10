33321ca90dabd04930aad3f7f803cc7020018dc6
kth <pni2396@gmail.com>
2026-08-10 13:39:11 +0900

    feat: 주문 생성 알림 전송 Command 변수명 변경

--- src/main/java/com/sparta/logistics/notification/application/command/dto/SendOrderNotificationCommand.java
+++ src/main/java/com/sparta/logistics/notification/application/command/dto/SendOrderNotificationCommand.java
@@ -4,7 +4,7 @@ import java.time.Instant;
 import java.util.UUID;
 
 public record SendOrderNotificationCommand(
-        UUID id,
+        UUID orderId,
         UUID departureHubId,
         UUID receiverCompanyId,
         UUID productId,
