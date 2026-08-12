512fda7d06325f489b1d651af850c6c30f830e0b
kth <pni2396@gmail.com>
2026-08-13 05:37:20 +0900

    infra: Swagger 상대 경로 설정 및 Bearer 헤더 정의

--- src/main/java/com/sparta/logistics/notification/common/config/SwaggerConfig.java
+++ src/main/java/com/sparta/logistics/notification/common/config/SwaggerConfig.java
@@ -1,6 +1,9 @@
 package com.sparta.logistics.notification.common.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,8 +15,24 @@ public class SwaggerConfig {
 
     @Bean
     public OpenAPI openAPI() {
+
+        // SecurityScheme 이름 정의
+        String jwtSchemeName = "jwtAuth";
+
+        // API 요청 시 SecurityRequirement 적용
+        SecurityRequirement securityRequirement = new SecurityRequirement().addList(jwtSchemeName);
+
+        // Bearer Token 방식의 Components 구성
+        Components components = new Components()
+                .addSecuritySchemes(jwtSchemeName, new SecurityScheme()
+                        .name(jwtSchemeName)
+                        .type(SecurityScheme.Type.HTTP)
+                        .scheme("bearer")
+                        .bearerFormat("JWT"));
         return new OpenAPI()
-                .servers(List.of(new Server().url("/"))) 
+                .servers(List.of(new Server().url("/"))) // Gateway 경로 유지를 위한 상대 경로 설정
+                .addSecurityItem(securityRequirement)
+                .components(components)
                 .info(new io.swagger.v3.oas.models.info.Info()
                         .title("Notification Service API")
                         .version("v1.0"));
