fc2d4d81e125e3d41bc2f84b18e6cda75e02a65b
kth <pni2396@gmail.com>
2026-08-13 14:50:05 +0900

    fix: UserService FeignClient Request DTO 수정
    - List<UUID> -> SearchUserRequest DTO

--- src/main/java/com/sparta/logistics/notification/infrastructure/feign/fallback/UserFeignClientFallbackFactory.java
+++ src/main/java/com/sparta/logistics/notification/infrastructure/feign/fallback/UserFeignClientFallbackFactory.java
@@ -2,6 +2,7 @@ package com.sparta.logistics.notification.infrastructure.feign.fallback;
 
 import com.sparta.logistics.notification.common.code.GeneralResponseCode;
 import com.sparta.logistics.notification.infrastructure.feign.client.UserFeignClient;
+import com.sparta.logistics.notification.infrastructure.feign.dto.SearchUsersRequest;
 import com.sparta.logistics.notification.infrastructure.feign.dto.UserResponse;
 import com.sparta.logistics.notification.presentation.common.dto.response.GeneralResponse;
 import lombok.extern.slf4j.Slf4j;
@@ -10,7 +11,6 @@ import org.springframework.stereotype.Component;
 
 import java.util.Collections;
 import java.util.List;
-import java.util.UUID;
 
 @Slf4j
 @Component
@@ -20,7 +20,7 @@ public class UserFeignClientFallbackFactory implements FallbackFactory<UserFeign
     public UserFeignClient create(Throwable cause) {
         return new UserFeignClient() {
             @Override
-            public GeneralResponse<List<UserResponse>> searchUsersById(List<UUID> userIds) {
+            public GeneralResponse<List<UserResponse>> searchUsersById(SearchUsersRequest request) {
                 // Feign 호출 실패 시 원인 예외 로깅
                 log.error("[UserFeignClient] searchUsersById 호출 실패 - cause: {}", cause.getMessage(), cause);
 
